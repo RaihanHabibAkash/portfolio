f08e1df1034df0a7373ec8376b83c951cee98d31
RaihanHabibAkash <raihanakash2003811@gmail.com>
2025-10-08 21:20:41 -0700

    Remove scroll event handling for section navigation and associated variables

--- assets/js/prime.js
+++ assets/js/prime.js
@@ -88,24 +88,6 @@ contact4.addEventListener("mouseout", event => {
     }
 });
 /* card end */
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(navLink => {
-                navLink.classList.remove("active");
-                document.querySelector('header nav a[href*=' + id + ']').classList.add("active");
-            });
-        }
-    })
-}
 
 
 
